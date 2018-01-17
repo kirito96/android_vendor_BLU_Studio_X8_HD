f169e4b2aa714ec70f3470edb9c4c1e695bb6623
kirito96 <aquammie96@gmail.com>
2018-01-16 22:26:49 -0400

    Update and rename irisX8-vendor.mk to Studio_X8_HD-vendor.mk

---
+++ irisX8/Studio_X8_HD-vendor.mk
@@ -0,0 +1,9 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/BLU/Studio_X8_HD/proprietary/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/BLU/Studio_X8_HD/proprietary/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/BLU/Studio_X8_HD/proprietary/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/BLU/Studio_X8_HD/proprietary/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/BLU/Studio_X8_HD/proprietary/vendor,system/vendor)
